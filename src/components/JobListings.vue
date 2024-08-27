db94182abe1285c6106b012657681fdd173635e3
imranaxix <imranfreelancer69@gmail.com>
2024-08-27 16:01:47 +0500

    first commit

---
+++ src/components/JobListings.vue
@@ -0,0 +1,67 @@
+<script setup>
+import JobListing from './JobListing.vue';
+import { ref,defineProps, onMounted } from 'vue';
+import axios from 'axios';
+import { RouterLink } from 'vue-router';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
+const jobs = ref([]);
+const loading = ref(true);
+
+defineProps({
+  limit: Number,
+  showButton: {
+    type: Boolean,
+    default: false,
+  },
+  isLoading: {
+    type: Boolean,
+    default: true,
+  }
+})
+
+onMounted(async () =>{
+ try{
+  const response = await axios.get("/api/jobs");
+  jobs.value = response.data;
+ }
+ catch{
+  console.log("Error Fetching data");
+ }
+ finally{
+  loading.value = false;
+ }
+})
+
+
+
+</script>
+
+
+<template>
+ 
+<section class="bg-blue-50 px-4 py-10">
+
+  <div class="container m-auto">
+    <h2 class="text-3xl font-bold text-green-500 text-center mb-6">Browse Jobs</h2>
+
+    <div v-if="loading" class="text-center text-gray-500 py-6">
+      <PulseLoader/>
+    </div>
+
+    <div v-if="!loading" class="grid grid-cols-1 md:grid-cols-3 gap-6">
+      <JobListing v-for="job in jobs.slice(0 , limit || jobs.length)" :key="job.id" :job="job"/>
+    </div>
+  </div>
+
+</section>
+
+
+<section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+      <RouterLink
+        to="/jobs"
+        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >View All Jobs
+      </RouterLink>
+    </section>
+
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Hero from '@/components/Hero.vue';
+import JobListings from '@/components/JobListings.vue';
+import HomeCard from '@/components/HomeCard.vue';
+</script>
+
+<template>
+  <Hero/>
+  <HomeCard/>
+  <JobListings :limit='3' :showButton="true"/>
+</template>
